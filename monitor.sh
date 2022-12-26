80eb6f2e48260681977a499487ef552c94696c15
bpekalp <b.pekalp@gmail.com>
2022-12-26 18:07:31 +0300

    Date added, sleep timer increased.

--- monitor.sh
+++ monitor.sh
@@ -8,10 +8,11 @@ SQL_PASSWORD="0000"
 SQL_TABLE="proje"
 
 while true; do
+    DATE=$(date)
     CHANGES=$(inotifywait -e modify,move,create,delete --format '%w%f %e' -r $DIRECTORY)
     if [ ! -z "$CHANGES" ]; then
-        psql -d "$SQL_NAME" -U "$SQL_USERNAME" -c "INSERT INTO $SQL_TABLE (event) VALUES ('$CHANGES');"
-        echo $CHANGES >>/home/gbpekalp/Masaüstü/projelog.log
+        psql -d "$SQL_NAME" -U "$SQL_USERNAME" -c "INSERT INTO $SQL_TABLE (event) VALUES ('$DATE    $CHANGES');"
+        echo $DATE $CHANGES >>/home/gbpekalp/Masaüstü/projelog.log
     fi
-    sleep 3
+    sleep 5
 done
