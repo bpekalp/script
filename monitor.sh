9bb82fbf08b37d51400e243f9d94c37f9700c1f0
bpekalp <b.pekalp@gmail.com>
2022-12-26 17:39:57 +0300

    Script now logs into a file aswell.

--- monitor.sh
+++ monitor.sh
@@ -11,6 +11,7 @@ while true; do
     CHANGES=$(inotifywait -e modify,move,create,delete --format '%w%f %e' -r $DIRECTORY)
     if [ ! -z "$CHANGES" ]; then
         psql -d "$SQL_NAME" -U "$SQL_USERNAME" -c "INSERT INTO $SQL_TABLE (event) VALUES ('$CHANGES');"
+        echo $CHANGES >>/home/gbpekalp/Masaüstü/projelog.log
     fi
     sleep 10
 done
